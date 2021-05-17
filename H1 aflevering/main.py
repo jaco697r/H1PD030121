2c7462f72d9e0c7c7bd892f6136982423b43dfb6
Jacob <jaco697r@elev.techcollege.dk>
2021-05-17 11:36:57 +0200

    [REF] Added more comments

--- H1 aflevering/main.py
+++ H1 aflevering/main.py
@@ -17,6 +17,7 @@ def login(connection):
             c_id = new_customer.create_user(cursor, connection)
             start_menu(c_id, cursor)
         else:
+            # Check if user_input is an integer.
             try:
                 int_user_input = int(user_input)
             except:
@@ -31,36 +32,40 @@ def login(connection):
                 else:
                     print("Customer ID not found. Try again or contact support")
 
-
+# Passing cursor for unittest purpose
 def start_menu(c_id, cursor):
     while True:
         print("Welcome to GODLIKE-ERP") 
         print("1. Buy products")
-        print("2. Add products to stock")
-        print("3. See all products")
-        print("4. See all customers")
+        print("2. See your basket")
+        print("3. Add products to stock")
+        print("4. See all products")
+        print("5. Delete product")
+        print("6. See all customers")
         print("8. Exit")
         
         user_input = input("\nWhat do you want to do?: ")
         if user_input == '1':
             basketlines.buy_products(c_id, connection)
         elif user_input == '2':
-            items.add_products(connection, cursor)
+            basketlines.see_basketlines(c_id)
         elif user_input == '3':
-            items.see_products(cursor)
+            items.add_products(connection, cursor)
         elif user_input == '4':
+            items.see_products(cursor)
+        elif user_input == '5':
+            items.delete_product(connection)
+        elif user_input == '6':
             customers.see_customers(cursor)
         elif user_input == '8':
             connection.close()
             exit()
 
-
 if __name__ == "__main__":
     try:
-        connection=sqlite3.connect("db.db")
+        connection=sqlite3.connect(os.getcwd()+"\H1 aflevering\db.db")
         # Passing connection for unittest purpose
         login(connection)
     finally:
         connection.close()
-
 
--- H1 aflevering/test.py
+++ H1 aflevering/test.py
@@ -1,13 +1,13 @@
 import unittest
 import sqlite3
-from main import create_basket
+from utils import basket
 
 class MyTest(unittest.TestCase):
     # Test if create_basket method returns basket id 1 when input is customerID 1
     def test(self):
         connection=sqlite3.connect("db.db")
-        cursor=connection.cursor()
-        self.assertEqual(create_basket(1, cursor), 1)       
+        self.assertEqual(basket.create_basket(1, connection), 1)
+        connection.close()
 
 if __name__ == '__main__':
     unittest.main()
